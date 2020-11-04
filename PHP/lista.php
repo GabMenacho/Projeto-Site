ad8d812a77a79a175d90eccc653b9285e6a76d2f
Gab Menacho <72589489+GabMenacho@users.noreply.github.com>
2020-11-04 00:15:00 -0300

    Lista dos produtos uma store

---
+++ PHP/lista.php
@@ -0,0 +1,20 @@
+<?php
+
+    require_once "db_connect.php";
+
+
+?>
+
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-5 m-auto">
+            <h3> Lista de Produtos</h3>
+                <table class="table table-responsive table-striped">
+                <tr>
+                    <th> Nome </th>
+
+
+<?php
+
+?>
