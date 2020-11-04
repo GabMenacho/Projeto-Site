4699b13a17751e4cf2c269a7904ceb69a209a3ed
Gab Menacho <72589489+GabMenacho@users.noreply.github.com>
2020-11-04 00:37:24 -0300

    Atualização lista dos produtos UmaStore em PHP

--- PHP/lista.php
+++ PHP/lista.php
@@ -1,6 +1,6 @@
 <?php
 
-    require_once "db_connect.php";
+    require_once "./acoes/db_connect.php";
 
 
 ?>
@@ -8,13 +8,55 @@
 
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-5 m-auto">
+            <div class="col-md-8 m-auto">
             <h3> Lista de Produtos</h3>
                 <table class="table table-responsive table-striped">
                 <tr>
+                    <th> Código </th>
                     <th> Nome </th>
+                    <th> Descrição </th>
+                    <th> Preço </th>
+                    <th> Imagem </th>
+                    <th> Opções </th>
+                </tr>
 
 
 <?php
+    $sql = "SELECT * FROM produtos";
+    $resultado = mysqli_query($connect, $sql);
+    while($row = mysqli_fetch_assoc($resultado)){
+?>
+
+                    <tr>
+                        <td>
+                            <?php echo $row['Cod_Produto']; ?> <!-- coloquei igual está na minha tabela sql-->
+                        </td>
+                        <td>
+                            <?php echo $row['nome']; ?>
+                        </td>
+                        <td>
+                            <?php echo $row['descricao']; ?>
+                        </td>
+                        <td>
+                            <?php echo $row['preco']; ?>
+                        </td>
+                        <td>
+                            <?php echo $row['nomeImagem']; ?>
+                        </td>
+                        <td>
+                            <a class="btn btn-sm btn-primary" href="./atualizar.php?atualizar=<?php echo $row['id']; ?>">Editar</a>|
+                            <a class="btn btn-sm btn-success" href="./detalhe.php?visualizar=<?php echo $row['id']; ?>">Detalhe</a>|
+                            <a class="btn btn-sm btn-danger" href="./actions/deletar_produtos.php?deletar=<?php echo $row['id']; ?>" >Deletar</a>
+                        </td>
+                    </tr>
+<?php
+
+    }
 
 ?>
+                    
+                </table>
+            </div>
+        </div>
+    </div>
+
