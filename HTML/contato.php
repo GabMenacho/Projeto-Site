0307de763dffe37cb9e74146e558d3550edca7e8
GabMenacho <menacho.gabriela@gmail.com>
2020-11-15 02:55:58 -0300

    formulario bootstrap atualizado

--- HTML/contato.php
+++ HTML/contato.php
@@ -14,57 +14,70 @@
             include_once "../PHP/includes/navegacao.php";
     ?>
 
-        <br>
-        <br>
+    <br>
+    <br>
 
-        <p>Gostaria de vender os seus trabalhos artísticos? Preencha nosso formulário e entraremos em contato!</p>
+    <p>Gostaria de vender os seus trabalhos artísticos? Preencha nosso formulário e entraremos em contato!</p>
 
-        <p>Formulário de interesse</p>
+    <p>Formulário de interesse</p>
                 
-            <div align ="center">
-                <form class="forminteresse">
-                 <ol type="1">
-                    <li><label for="pname">Nome:</label></li>
-          
-                    <input type="text" id="pname" name="fnome"><br><br>
+            <form>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label for="pname" >Nome:</label>
+                        <input type="text" class="form-control" id="pname" name="fnome">
+                    </div>
 
-                    <li><label for="pgenero">Gênero:</label></li>
-                    <input type="radio" id="feminino" name="genero" value="feminino-selecionado"><label for="feminino">Feminino</label>
-                    <br>
-	                <input type="radio" id="masculino" name="genero" value="masculino-selecionado"><label for="masculino">Masculino</label>
-                    <br>
-                    <input type="radio" id="outros" name="genero" value="outros-selecionado" checked><label for="outros">Outros</label>  
-	                <br>
-	                <br>
+                    <div class="form-group">
+                    <label for="pgenero">Gênero:</label><br>
+                    <input type="radio" id="feminino" class="form-radio-input" name="genero" value="feminino-selecionado">
+                    <label for="feminino" class="form-radio-label">Feminino</label><br>
+                    <input type="radio" id="masculino" name="genero" value="masculino-selecionado">
+                    <label for="masculino">Masculino</label><br>
+                    <input type="radio" id="outros" name="genero" value="outros-selecionado">
+                    <label for="outros">Outros</label>  
+                    </div>
+                    
+                    <div class = "form-group">
+                    <label for="mail">Cidade:</label>
+                    <input type="text" id="cidade" class="form-control" name="city">
+                    </div>
 
-                    <li><label for="mail">Cidade:</label></li>
-          
-                    <input type="text" id="cidade" name="city"><br><br>
-          
-                    <li><label for="num">Número de telefone:</label></li>
-          
-                    <input type="number" id="num" name="num"><br><br>
+                    <div class = "form-group">
+                    <label for="num">Número de telefone:</label>
+                    <input type="number" id="num" class="form-control" name="num">
+                    </div>
 
-                    <li><label for="mail">E-mail:</label></li>
-          
-                    <input type="mail" id="mail"><br><br>
+                    <div class = "form-groupd">
+                    <label for="mail">E-mail:</label>
+                    <input type="email" id="mail" class="form-control">
+                    </div>
 
-                    <li><label>Quais tipos de arte você cria?<br>Marque sua(s) resposta(s).</label></li>
-                    <input type="checkbox" id="ilustracao" name="ilustracao" value="ilustracao"><label for="ilustracao">Ilustração (digital, pintura, mão livre, quadrinhos)</label><br>
-                    <input type="checkbox" id="escultura" name="escultura" value="escultura"><label for="escultura">Escultura (diversos)</label><br>
-                    <input type="checkbox" id="objetos" name="objetos" value="objetos"><label for="objetos">Objetos lúdicos</label><br>
-                    <input type="checkbox" id="outros" name="outros" value="outros"><label for="outros">Outros</label> <input type="text" id="escreva" name="escreva" value="Especifique"><br><br>
-                    <br><br>
+                    <div class = "form-group form-check">
+                    <label>Quais tipos de arte você cria?<br>Marque sua(s) resposta(s).</label><br>
+                    
+                    <input type="checkbox" id="ilustracao" class = "form-check-input" name="ilustracao" value="ilustracao">
+                    <label for="ilustracao" class = "form-check-label" >Ilustração (digital, pintura, mão livre, quadrinhos)</label><br>
+                    
+                    <input type="checkbox" id="escultura" class = "form-check-input" name="escultura" value="escultura">
+                    <label for="escultura" class = "form-check-label">Escultura (diversos)</label><br>
+                    
+                    <input type="checkbox" id="objetos" class = "form-check-input" name="objetos" value="objetos">
+                    <label for="objetos" class = "form-check-label">Objetos lúdicos</label><br>
+
+                    <input type="checkbox" id="outros" class = "form-check-input" name="outros" value="outros">
+                    <label for="outros" class = "form-check-label">Outros</label>
+                    <input type="text" id="escreva" name="escreva" value="Especifique">
+                    </div>
           
-                    <li><label for="msg">Conte-nos um pouco sobre você e seus trabalhos:</label></li>
-          
+                    <div class = "form-group">
+                    <label for="msg">Conte-nos um pouco sobre você e seus trabalhos:</label><br>
                     <textarea id="msg" rows="10" cols="50"></textarea><br><br>
+                    </div>
           
-          
-                    <button onclick="botaoEnviar()">Enviar</button>
-                    </ol>
-                </form>
-            </div>
+                    <button type = "submit" class="btn btn-primary" onclick="botaoEnviar()">Enviar</button>
+                </div>
+            </form>
       <br>
 
       <hr>
